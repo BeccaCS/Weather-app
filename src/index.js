ee116fe922d33daabb84b475d275aea7fcdd987b
Rebecca Seepaul <150277065+BeccaCS@users.noreply.github.com>
2024-01-10 15:40:03 -0400

    updated search engine and API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-let apiKey = "6cb9ba6b06o8d48e9btcf36869da4a46";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+function updatedWeather(response) {
+  let temperature = document.querySelector("#display-degrees");
+  let cityElement = document.querySelector("#city");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+
+  cityElement.innerHTML = response.data.city;
+}
+
+function search(city) {
+  let apiKey = "6cb9ba6b06o8d48e9btcf36869da4a46";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updatedWeather);
+}
 
 function formSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+
+  search(searchInput.value);
 }
 
 let searchform = document.querySelector("#search-form-input");
 searchform.addEventListener("submit", formSearch);
+
+/*
+function currentDate(date) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+}
+*/
